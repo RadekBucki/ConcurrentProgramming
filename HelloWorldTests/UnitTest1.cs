0f490c23d0e81989863c5918d01f419a6156c4e2
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2022-03-24 19:25:09 +0100

    Add method implemented and tested

--- HelloWorldTests/UnitTest1.cs
+++ HelloWorldTests/UnitTest1.cs
@@ -6,9 +6,16 @@ namespace HelloWorldTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void HelloWorldTest()
         {
             Assert.AreEqual("Hello world", HelloWorld.Class1.GetHelloWorld());
+        }
+
+        [TestMethod]
+        public void AddMethodTest()
+        {
+            Assert.IsNotNull(HelloWorld.Class1.Add(1, 2));
+            Assert.AreEqual(3,HelloWorld.Class1.Add(1, 2));
         }
     }
 }
