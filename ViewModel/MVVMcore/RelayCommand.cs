d91f3a6c9120c7cb0d33d4ba44a7f7e631d2e7f3
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2022-04-02 17:22:29 +0200

    mvvm gui implemented

---
+++ ViewModel/MVVMcore/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Input;
+
+namespace Presentation.ViewModel.MVVMCore
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public RelayCommand(Action execute) : this(execute, null) { }
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if (this.canExecute == null)
+                return true;
+            if (parameter == null)
+                return this.canExecute();
+            return this.canExecute();
+        }
+
+        public void Execute(object? parameter)
+        {
+            this.execute();
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        internal void OnCanExecuteChanged()
+        {
+            this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,84 @@
+﻿using Presentation.ViewModel.MVVMCore;
+
+namespace Presentation.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            StartCommand = new RelayCommand(this.StartBalls, this.CanDoDisableButton);
+            StopCommand = new RelayCommand(this.StopBalls, this.CanDoEnableButton);
+            m_NumOfBalls = "";
+        }
+
+        public RelayCommand StartCommand
+        {
+            get;
+        }
+
+        public RelayCommand StopCommand
+        {
+            get;
+        }
+
+        private bool m_buttonEnabled = true;
+        public bool ButtonEnabled
+        {
+            get { return m_buttonEnabled; }
+            set
+            { 
+                m_buttonEnabled = value; 
+                OnPropertyChanged();
+            }
+        }
+
+        private string m_NumOfBalls;
+        public string NumOfBalls
+        {
+            get { return m_NumOfBalls; }
+            set { m_NumOfBalls = value; OnPropertyChanged(); }
+        }
+        private void StartBalls()
+        {
+            try
+            {
+                int ballsNum = Int32.Parse(NumOfBalls);
+                if (ballsNum > 0)
+                {
+                    DoChangeButtonEnabled();
+                }
+                else
+                {
+                    throw new ArgumentException("Not an positive integer");
+                }
+            }
+            catch (Exception ex)
+            {
+                NumOfBalls = "";
+                OnPropertyChanged();
+            }
+        }
+
+        private void StopBalls()
+        {
+            DoChangeButtonEnabled();
+        }
+
+        private void DoChangeButtonEnabled()
+        {
+            ButtonEnabled = !ButtonEnabled;
+            StartCommand.OnCanExecuteChanged();
+            StopCommand.OnCanExecuteChanged();
+        }
+
+        private bool CanDoDisableButton()
+        {
+            return ButtonEnabled;
+        }
+
+        private bool CanDoEnableButton()
+        {
+            return !ButtonEnabled;
+        }
+    }
+}
